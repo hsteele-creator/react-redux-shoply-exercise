1a454d3e010d175d4702bf0c3dbee871b43001a1
Harry Steele <harrysteele@Harrys-MacBook-Air.local>
2023-01-12 13:33:48 -0500

    updated reducer

--- src/Cart.js
+++ src/Cart.js
@@ -1,39 +1,45 @@
-import React from "react"
-import CartItem from "./CartItem"
-import { useSelector } from "react-redux"
-import "./css/Cart.css"
-import Nav from "./Nav"
+import React from "react";
+import CartItem from "./CartItem";
+import { useSelector } from "react-redux";
+import "./css/Cart.css";
+import Nav from "./Nav";
 
 const Cart = () => {
+  const cart = useSelector((state) => state.cart);
+  console.log(cart);
 
-    const cart = useSelector(state => state.cart);
-    console.log(cart)
+  return (
+    <>
+      <Nav />
+      <h1 id="cart-title">Cart</h1>
 
-    
+      <h2 id="total">
+        Cart Total : $
+        {cart.length > 0
+          ? cart.reduce(
+              (curr, next) =>
+                curr + (next.price * next.quantity), 0
+            )
+          : 0}
+      </h2>
 
+      <div id="cart-container">
+        {cart
+          .filter((product) => product.quantity != 0)
+          .map((product) => {
+            return (
+              <CartItem
+                name={product.name}
+                price={product.price}
+                description={product.description}
+                image={product.image_url}
+                quantity={product.quantity}
+              />
+            );
+          })}
+      </div>
+    </>
+  );
+};
 
-    return (
-        <>
-
-        <Nav/>
-        <h1 id="cart-title">Cart</h1>
-
-        {/* <h2 id="total">Cart Total : ${cart.length > 0 ? cart.reduce((curr, next) => (curr.price * curr.quantity) + (next.price * next.quantity)) : 0}</h2> */}
-
-        <div id="cart-container">
-        {cart.filter(product => product.quantity != 0).map(product => {
-            return <CartItem 
-            name={product.name}
-            price={product.price}
-            description={product.description}
-            image={product.image_url}
-            quantity={product.quantity}
-            />
-        })}
-        </div>
-        </>
-    )
-
-}
-
-export default Cart
+export default Cart;
--- src/rootreducer.js
+++ src/rootreducer.js
@@ -1,42 +1,70 @@
-import React from "react"
-import { act } from "react-dom/test-utils"
-import data from "./data.json"
+import React from "react";
+import { act } from "react-dom/test-utils";
+import data from "./data.json";
 
-const INITIAL_STATE = {products : data.products, cart: []}
-
+const INITIAL_STATE = { products: data.products, cart: [] };
 
 const rootReducer = (state = INITIAL_STATE, action) => {
-    switch (action.type) {
-        case("ADD_TO_CART"):
-            if(state.cart.some(product => product.name === action.payload.name)) {
-                return {
-                    ...state, 
-                    cart : [...state.cart.map(product => product.name === action.payload.name ? {...product, quantity : product.quantity + action.payload.quantity} : product)]
-                }
-            } else {
-                return {
-                    ...state,
-                    cart : [...state.cart, {...data.products[action.payload.id], quantity : action.payload.quantity}]
-                }
-            }
-        case("REMOVE_FROM_CART"):
-            return {
-                ...state, 
-                cart : [...state.cart.filter(product => product.name != action.payload.name)]
-            }
-        case("RAISE_QAUNTITY"):
-            return {
-                ...state, 
-                cart : [...state.cart.map(product => product.name === action.payload.name ? {...product, quantity : product.quantity + 1} : product)]
-            }    
-        case("LOWER_QUANTITY"):
-            return {
-                ...state, 
-                cart : [...state.cart.map(product => product.name === action.payload.name ? {...product, quantity : product.quantity - 1} : product)]
-            }       
-        default: 
-            return state
-    }
-}
+  switch (action.type) {
+    case "ADD_TO_CART":
+      if (state.cart.some((product) => product.name === action.payload.name)) {
+        const product = state.cart.map((item) => {
+          if (item.name === action.payload.name) {
+            item.quantity = item.quantity + action.payload.quantity
+          } 
+          return item
+        });
 
-export default rootReducer
+        return {
+          ...state,
+          cart: [...product],
+        };
+      } else {
+        return {
+          ...state,
+          cart: [
+            ...state.cart,
+            {
+              ...data.products[action.payload.id],
+              quantity: action.payload.quantity,
+            },
+          ],
+        };
+      }
+    case "REMOVE_FROM_CART":
+      return {
+        ...state,
+        cart: [
+          ...state.cart.filter(
+            (product) => product.name != action.payload.name
+          ),
+        ],
+      };
+    case "RAISE_QAUNTITY":
+      return {
+        ...state,
+        cart: [
+          ...state.cart.map((product) =>
+            product.name === action.payload.name
+              ? { ...product, quantity: product.quantity + 1 }
+              : product
+          ),
+        ],
+      };
+    case "LOWER_QUANTITY":
+      return {
+        ...state,
+        cart: [
+          ...state.cart.map((product) =>
+            product.name === action.payload.name
+              ? { ...product, quantity: product.quantity - 1 }
+              : product
+          ),
+        ],
+      };
+    default:
+      return state;
+  }
+};
+
+export default rootReducer;
